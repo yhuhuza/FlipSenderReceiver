ccbfcfc8452569cf30ba8d3aa0aec78dd2c1c9cf
Nikolai Maidan <axroit.08@gmail.com>
2022-12-27 23:38:54 +0400

    add test code and styles

--- src/content/app.js
+++ src/content/app.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import './index.scss';
+
+const $app = document.querySelector('#app');
+
+const html = `
+  <div class='hello'>
+    <div>
+        Hello Dima!
+    </div>
+  </div>
+`
+
+$app.insertAdjacentHTML('beforeend', html);
