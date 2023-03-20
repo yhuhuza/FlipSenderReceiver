a0dbca7ce37c9fa9d8ce3eed34ea27d2f5469d47
Nikolai Maidan <axroit.08@gmail.com>
2023-03-20 21:51:31 +0400

    add changes to main file

---
+++ src/popup/FlipReciever.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './FlipReciever.scss';
+
+function FlipReciever() {
+
+  return (
+      <div className={'kaka'}>KAKASHKA</div>
+  );
+}
+
+export default FlipReciever;
---
+++ src/popup/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import FlipReciever from "./FlipReciever.jsx";
+
+console.log('OPACHA');
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+      <FlipReciever />
+);
+
+
